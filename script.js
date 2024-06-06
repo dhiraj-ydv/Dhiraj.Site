5a761febf30d7e491c4e89bd15745a1ee65e6b83
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2024-06-06 18:44:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//for navbar
 const navigationHeight = document.querySelector('.navbar').offsetHeight;
         document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
 
-
+//code for service-worker to clear browser cache
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
         .then((registration) => {
@@ -10,4 +11,28 @@ if ('serviceWorker' in navigator) {
             console.log('Service Worker registration failed:', error);
         });
 }
+
+//for form data submission to database
+$(document).ready(function() {
+            $('#contactForm').on('submit', function(event) {
+                event.preventDefault();
+                const name = $('#name').val();
+                const email = $('#email').val();
+                const message = $('#message').val();
+
+                $.ajax({
+                    url: '/submit',
+                    method: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify({ name: name, email: email, message: message }),
+                    success: function(response) {
+                        $('#result').text('Form submitted successfully!');
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('Error:', error);
+                        $('#result').text('Form submission failed.');
+                    }
+                });
+            });
+        });
 
